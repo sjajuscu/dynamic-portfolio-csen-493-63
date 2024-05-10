aa1d1bee585d1364564682aece572f50a0d7b75a
Suyash Jaju <sjaju@scu.edu>
2024-05-10 12:54:38 -0700

    Spring Initialize

---
+++ DPG/src/test/java/CSEN275/DPG/DpgApplicationTests.java
@@ -0,0 +1,13 @@
+package CSEN275.DPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
